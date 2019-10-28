56164d2c71262b4c60430ff6846ed4c3ff9375d9
ABlackTaco <ablacktaco@gmail.com>
2019-10-28 09:59:12 +0800

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by 陳姿穎 on 2019/10/28.
+//  Copyright © 2019 陳姿穎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
